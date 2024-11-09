9ece4d5b416b934be4b1530f99555109f218c2c9
Lautaro-io <ildarrazlautaro@gmail.com>
2024-11-09 14:58:20 -0300

    Update

--- src/gui/App.py
+++ src/gui/App.py
@@ -1,3 +1,5 @@
+from tkinter import PhotoImage
+
 import requests
 import json
 import customtkinter as ctk
@@ -17,20 +19,37 @@ class App(ctk.CTk):
 
         x = (screen_width // 2) - (ancho // 2)
         y = (screen_height // 2) - (alto // 2)
+        """""//////////////////////////////////// ICONOS //////////////////////////////////////////"""
+        sunIcon = PhotoImage(file="src/assets/soleado.png")
+        # waterIcon = PhotoImage(file="src/assets/soleado.png")
+        # celsiusIcon = PhotoImage(file="src/assets/soleado.png")
+        # nightIcon = PhotoImage(file="src/assets/soleado.png")
+        # nightNubIcon = PhotoImage(file="src/assets/soleado.png")
+        # nubIcon = PhotoImage(file="src/assets/soleado.png")
+        # vientoNubIcon = PhotoImage(file="src/assets/soleado.png")
+        # nubIcon = PhotoImage(file="src/assets/soleado.png")
+        # stormIcon = PhotoImage(file="src/assets/soleado.png")
+
+
+
 
         self.geometry(f"{ancho}x{alto}+{x}+{y}")
-
-        self.title = ctk.CTkLabel(self, text="Weather App beta", font=("Roboto", 30, "bold"))
-        self.title.pack(pady=10)
-
-        self.city = ctk.CTkEntry(self, placeholder_text="Elige una ciudad: EJ: Miramar,AR ", width=250)
-        self.city.pack(pady=10)
+        self.configure(fg_color="steelblue")
+        self.title = ctk.CTkLabel(self, text="""WeatherApp
+        beta""", font=("Roboto", 30, "bold"))
+        self.resizable(False,False)
+        self.title.grid(row =0 , column = 0, pady=10)
+        self.grid_columnconfigure(0 , weight= 1)
+        self.grid_columnconfigure(2 , weight= 1)
+        self.grid_columnconfigure(3 , weight= 1)
+        self.city = ctk.CTkEntry(self, placeholder_text="Elige una ciudad: EJ: Miramar,AR ", width=200)
+        self.city.grid(row = 2 , column = 0,pady=10,padx = 75)
         self.frameOptions = ctk.CTkFrame(self, width=200, height=50)
-        self.frameOptions.pack(pady=5)
+        self.frameOptions.grid(row = 3 , column = 0,pady=5)
         self.labelTemp = ctk.CTkLabel(self.frameOptions, text="")
-        self.labelTemp.pack(pady=10)
+        self.labelTemp.grid(row = 0 , column = 0, pady=10)
         self.submitBtn = ctk.CTkButton(self, text="See weather", command=self.see_temp)
-        self.submitBtn.pack(pady=10)
+        self.submitBtn.grid( row = 4, column = 0 , pady=10 , padx = 75)
 
     def see_temp(self):
         api_key = "928e9266acd34ebd98f142154240911  "
